43c818fa129e642c16306d464e4d73a5bdd2ab95
Martins Otankis <mo@fair.no>
2021-01-24 15:19:02 +0200

    Front page layout idea

--- server/index.js
+++ server/index.js
@@ -26,6 +26,6 @@ app.get('/', async (req, res) => {
     }
 });
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+app.listen(3005, ()=>{
+    console.log("Server running on port 3005");
 });
